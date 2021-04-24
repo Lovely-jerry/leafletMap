3a13a81e9dcd5427230772a6be3485d7c829243c
xiaoming <1658471647@qq.com>
2021-04-24 22:57:48 +0800

    实现更新地图上的点位数据（点击更新地图上显示序号）

--- src/pages/component/MapView.jsx
+++ src/pages/component/MapView.jsx
@@ -1,38 +1,26 @@
 import { Component } from 'react';
 import { Cards } from "./Cards.jsx";
-import './MapView.css'
+import { cloneDeep } from 'lodash'
+import './MapView.less'
 
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 import 'leaflet.markercluster';
 import 'leaflet.markercluster/dist/MarkerCluster.css'
 import 'leaflet.markercluster/dist/MarkerCluster.Default.css'
-
-//解决默认maker无法显示的问题
-import icon from 'leaflet/dist/images/marker-icon.png';
-import iconShadow from 'leaflet/dist/images/marker-shadow.png'
-
 import { tiledMapLayer } from "@supermap/iclient-leaflet";
 
 import fire from '../../assets/fire.png';
 import thunderbolt from '../../assets/thunderbolt-fill.png'
 
-
-// import smile from '../../assets/smile-filling.png'
-
 export default class MapView extends Component {
-
   constructor(props) {
     super(props)
     this.state = {
-      address: [
-        { "name": "人民大会堂", "lat": "39.903244", "lng": "116.387400", "type": "viewSpot" },
-        { "name": "天安门", "lat": "39.907064", "lng": "116.391305", "type": "viewSpot" },
-        { "name": "故宫", "lat": "39.916462", "lng": "116.390790", "type": "viewSpot" },
-        { "name": "国家博物馆", "lat": "39.903717", "lng": "116.395339", "type": "viewSpot" },
-        { "name": "北海公园", "lat": "39.923973", "lng": "116.383323", "type": "park" },
-        { "name": "景山公园", "lat": "39.924144", "lng": "116.390533", "type": "park" }
-      ]
+      //是否显示showDate1
+      isShowBigEl: true,
+      //地图对象
+      map: null          
 
     }
   }
@@ -41,44 +29,54 @@ export default class MapView extends Component {
     this.initMap()
   }
 
+  componentWillReceiveProps(nextProps) {
+    console.log('nextProps', nextProps);
+    //创建聚合点位
+    this.createClusterMarker(nextProps.address)
+  }
+
   initMap = () => {
     let host = 'https://iserver.supermap.io';
     let url = host + '/iserver/services/map-china400/rest/maps/China';
+    const { address } = this.props;
 
     //初始化地图
     let map = L.map('map', {
       center: [39.9, 116.4],    //北京市的经纬度
       maxZoom: 18,
-      zoom: 13
+      zoom: 10
     });
 
+    //移动地图收起大的元素
+    map.on('moveend', () => {
+      this.setState({
+        isShowBigEl: false
+      })
+    })
 
     //添加图层
     tiledMapLayer(url, { noWrap: true }).addTo(map);
 
-    //创建聚合点位
-    this.createClusterMarker(map)
-
-
+    this.setState({
+      map
+    }, () => {
+      //创建聚合点位
+      this.createClusterMarker(address)
+    })
 
   }
+
+
 
   /**
    * 创建聚合点位图层
    * @param {Obj} map 地图对象
    */
-  createClusterMarker = (map) => {
-    const { address } = this.state;
+  createClusterMarker = (address) => {
+    const { map } = this.state;
 
-    let fireIcon = L.icon({
-      iconUrl: fire,
-      iconSize: [20, 20]
-    })
-    let thunderboltIcon = L.icon({
-      iconUrl: thunderbolt,
-      iconSize: [20, 20]
-    })
-
+    console.log('address', address);
+    let { fireIcon, thunderboltIcon } = this.defineIconTypes()
 
     // 实例化markerCluster
     let cluster = L.markerClusterGroup({
@@ -86,7 +84,7 @@ export default class MapView extends Component {
     });
     let marker, icon = fireIcon;
 
-    address.forEach(item => {
+    address.forEach((item, index) => {
       let latLng = [item.lat, item.lng];
 
       if (item.type !== 'viewSpot') {
@@ -94,9 +92,12 @@ export default class MapView extends Component {
       }
 
       marker = L.marker(latLng, { icon });
-      item.type === 'viewSpot' ?
-        this.handlerMouseOverIcon(marker, latLng, map) :
-        this.handleOtherModle(marker,latLng, map)
+
+      this.handlerMouseOverIcon(marker, item.showIndex, latLng, map)
+
+      // item.type === 'viewSpot' ?
+      //   this.handlerMouseOverIcon(marker, index,latLng, map) :
+      //   this.handleOtherModle(marker, latLng, map)
 
       cluster.addLayer(marker)
     })
@@ -111,7 +112,9 @@ export default class MapView extends Component {
    * @param {} latLng 经纬度
    * @param {*} map 地图图层
    */
-  handlerMouseOverIcon = (marker, latLng, map) => {
+  handlerMouseOverIcon = (marker, showIndex, latLng, map) => {
+
+    this.createTooltipEl(showIndex, marker)
 
     marker.on('mouseover', () => {
       this.createPopupModule(latLng, map)
@@ -119,12 +122,47 @@ export default class MapView extends Component {
 
   }
 
-  handleOtherModle = (marker, latLng, map) => {
-    let myIcon = L.divIcon({ html:'<div>111</div>',className:'my-div-icon' });
-    L.marker(latLng, { icon: myIcon }).addTo(map)
-    // marker.addLayer()
+  /**
+   * 创建小弹窗样式
+   * @param {} Index 
+   * @param {Object} marker 图标图层
+   * @returns 
+   */
+  createTooltipEl(showIndex, marker) {
+    // 设置显示的序号
+    let index = showIndex > 9 ? `${showIndex}` : `0${showIndex}`;
+    //创建序号元素
+    showIndex !== -1 && marker.bindTooltip(index, {
+      permanent: true,
+      direction: 'top',
+      offset: [0, -10]
+    }).openTooltip();
+
+    return marker
   }
 
+  /**
+   * 定义图标种类，并将其返回
+   * @returns Object 图标对象组
+   */
+  defineIconTypes() {
+    let fireIcon = L.icon({
+      iconUrl: fire,
+      iconSize: [20, 20]
+    })
+    let thunderboltIcon = L.icon({
+      iconUrl: thunderbolt,
+      iconSize: [20, 20]
+    })
+
+    return { fireIcon, thunderboltIcon }
+  }
+
+  /**
+   * 创建小弹窗元素，并为其绑定点击事件
+   * @param {Array} latLng 经纬度数组
+   * @param {Object} map 地图对象
+   */
   createPopupModule = (latLng, map) => {
     let popup = L.popup({
       minWidth: 300,
@@ -142,13 +180,51 @@ export default class MapView extends Component {
   }
 
 
+  handleOtherModle = (marker, latLng, map) => {
+    let myIcon = L.divIcon({ html: '<div>111</div>', className: 'my-div-icon' });
+    L.marker(latLng, { icon: myIcon }).addTo(map)
+    // marker.addLayer()
+  }
+  /**
+   * 点击小元素显示大的div元素
+   */
+  handleShowBigEl = () => {
+    this.setState({
+      isShowBigEl: true
+    })
+  }
+
+  handleSetNewDate = (date) => {
+    const { dataProcessing } = this.props;
+    dataProcessing(date);
+  }
+
   render() {
+
+    let newDate = [
+      { "name": "景山公园", "lat": "39.924144", "lng": "116.390533", "type": "park" },
+      { "name": "故宫", "lat": "39.916462", "lng": "116.390790", "type": "viewSpot" },
+      { "name": "天安门", "lat": "39.907064", "lng": "116.391305", "type": "viewSpot" },
+      { "name": "北海公园", "lat": "39.923973", "lng": "116.383323", "type": "park" },
+      { "name": "国家博物馆", "lat": "39.903717", "lng": "116.395339", "type": "viewSpot" },
+      { "name": "人民大会堂", "lat": "39.903244", "lng": "116.387400", "type": "viewSpot" }
+    ]
+
+    const { isShowBigEl } = this.state;
     return (
       <div style={{ width: '100%', height: '100%', position: 'relative' }}>
         <div style={{ width: '100%', height: '100%' }} id="map"></div>
-        {/* <div style={{ width: '400px', height: '100%', position: 'absolute', right: 0, top: 0, backgroundColor: '#eea' }} /> */}
+        <div className='divEl'>
+          <button onClick={()=>{this.handleSetNewDate(newDate)}}>更新数据</button>&nbsp;&nbsp;
+          <button onClick={()=>{this.handleSetNewDate([])}}>重置数据</button>
+        </div>
+        <div className='showDateEl'>
+          {isShowBigEl ?
+            <div style={{ height: '600px' }}>showData</div> :
+            <div style={{ height: '50px' }} onClick={this.handleShowBigEl}>showData---dddd</div>
+          }
+        </div>
       </div>
     )
-
   }
-}
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,63 @@
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom';
+import { cloneDeep } from 'lodash'
 import styles from './index.css';
 import MapView from "@/pages/component/MapView";
 
-export default function () {
-  return (
-    <div className={styles.normal}>
-      <MapView />
-    </div>
-  );
+export default class Index extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      address: [
+        { "name": "人民大会堂", "lat": "39.903244", "lng": "116.387400", "type": "viewSpot", 'showIndex': -1 },
+        { "name": "天安门", "lat": "39.907064", "lng": "116.391305", "type": "viewSpot", 'showIndex': -1 },
+        { "name": "故宫", "lat": "39.916462", "lng": "116.390790", "type": "viewSpot", 'showIndex': -1 },
+        { "name": "国家博物馆", "lat": "39.903717", "lng": "116.395339", "type": "viewSpot", 'showIndex': -1 },
+        { "name": "北海公园", "lat": "39.923973", "lng": "116.383323", "type": "park", 'showIndex': -1 },
+        { "name": "景山公园", "lat": "39.924144", "lng": "116.390533", "type": "park", 'showIndex': -1 },
+        { "name": "北京通州", "lat": "39.866540", "lng": "116.663230", "type": "park", 'showIndex': -1 },
+        { "name": "怀柔", "lat": "40.245308", "lng": "116.690163", "type": "park", 'showIndex': -1 },
+        { "name": "大兴", "lat": "39.660475", "lng": "116.387191", "type": "park", 'showIndex': -1 }
+      ]
+    }
+  }
+
+
+  /**
+   * 对查询到的数据进行处理
+   * @param {*} newAddress 
+   */
+  dataProcessing = (newAddress) => {
+    const { address } = this.state;
+    let obj = {}, tempAddress = cloneDeep(address), tempNewAddress = cloneDeep(newAddress);
+
+    tempNewAddress.forEach((item, index) => {
+      obj[item.name] = index;
+    })
+
+    let nameList = Object.keys(obj);
+
+    tempAddress.forEach((item) => {
+      if (nameList.includes(item.name)) {
+        item.showIndex = obj[item.name]
+      } else {
+        item.showIndex = -1
+      }
+    })
+
+    console.log('newaddress', tempAddress);
+    this.setState({
+      address: tempAddress
+    })
+  }
+
+  render() {
+    const { address } = this.state;
+    return (
+      <div className={styles.normal}>
+        <MapView address={address} dataProcessing={this.dataProcessing} />
+      </div>
+    );
+  }
 }
